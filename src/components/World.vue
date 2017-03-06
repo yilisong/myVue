5543955a249387bee96a57c62e3714cbbd50c716
yilisong <yilisong@yundun.com>
2017-03-06 15:19:55 +0800

    test

---
+++ src/components/World.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="world">
+		<h3>{{msg}}</h3>
+	</div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'world',
+  data () {
+    return {
+      msg: 'this is a page of world!'
+    }
+  }
+}
+</script>
+
+<style type="text/css" scoped>
+	.world h3{
+		color: skyblue;
+	}
+</style>
